2250bb23a8636061461dc6ea05a5d27337a609fc
mondhermallek <mondher.mallek1@esprit.tn>
2021-03-04 21:26:36 +0100

    final

---
+++ app/src/main/java/com/esprit/scannerqrcode/MainActivity.kt
@@ -0,0 +1,212 @@
+package com.esprit.scannerqrcode
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.esprit.MQTT.MqttClientHelper
+import com.google.zxing.integration.android.IntentIntegrator
+//import kotlinx.android.synthetic.main.content_main.*
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended
+import org.eclipse.paho.client.mqttv3.MqttException
+import org.eclipse.paho.client.mqttv3.MqttMessage
+import java.util.*
+import kotlin.concurrent.schedule
+
+
+import org.eclipse.paho.android.service.MqttAndroidClient
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val mqttClient by lazy {
+        MqttClientHelper(this)
+    }
+
+
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK){
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if (result != null) {
+                if (result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+                } else {
+
+                   // btnPub.setOnClickListener { view ->
+                       // var snackbarMsg: String
+                        //val topic = editTextPubTopic.text.toString().trim()
+                        //snackbarMsg = "Cannot publish to empty topic!"
+                        //if (topic.isNotEmpty()) {
+                           // snackbarMsg = try {
+                               // mqttClient.publish(topic, result.contents)
+                                //result.contents, Toast.LENGTH_LONG
+                               // "Published to topic '$topic'"
+                            //} catch (ex: MqttException) {
+                               // "Error publishing to topic: $topic"
+                           // }
+                        //}
+                       // Snackbar.make(view, snackbarMsg, 300)
+                                //.setAction("Action", null).show()
+                    //}
+                    mqttClient.publish("test", result.contents)
+                    Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+    }
+
+    //bar code function
+
+   /* override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK){
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if (result != null) {
+                if (result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+    }
+*/
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textViewMsgPayload.movementMethod = ScrollingMovementMethod()
+
+
+        btn_scan.setOnClickListener {
+            val scanner = IntentIntegrator(this)
+            scanner.initiateScan()
+        }
+
+
+        setMqttCallBack()
+        // initialize 'num msgs received' field in the view
+        textViewNumMsgs.text = "0"
+
+
+
+       /* // pub button
+        btnPub.setOnClickListener { view ->
+            var snackbarMsg: String
+            val topic = editTextPubTopic.text.toString().trim()
+            snackbarMsg = "Cannot publish to empty topic!"
+            if (topic.isNotEmpty()) {
+                snackbarMsg = try {
+                    mqttClient.publish(topic, editTextMsgPayload.text.toString())
+                    //result.contents, Toast.LENGTH_LONG
+                    "Published to topic '$topic'"
+                } catch (ex: MqttException) {
+                    "Error publishing to topic: $topic"
+                }
+            }
+            Snackbar.make(view, snackbarMsg, 300)
+                    .setAction("Action", null).show()
+
+        }*/
+
+
+        // sub button
+        /*btnSub.setOnClickListener { view ->
+            var snackbarMsg: String
+            val topic = editTextSubTopic.text.toString().trim()
+            snackbarMsg = "Cannot subscribe to empty topic!"
+            if (topic.isNotEmpty()) {
+                snackbarMsg = try {
+                    mqttClient.subscribe(topic)
+                    "Subscribed to topic '$topic'"
+                } catch (ex: MqttException) {
+                    "Error subscribing to topic: $topic"
+                }
+            }
+            Snackbar.make(view, snackbarMsg, Snackbar.LENGTH_SHORT)
+                    .setAction("Action", null).show()
+        }*/
+
+        Timer("CheckMqttConnection", false).schedule(3000) {
+            if (!mqttClient.isConnected()) {
+                Snackbar.make(textViewNumMsgs, "Failed to connect to: '$SOLACE_MQTT_HOST' within 3 seconds", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Action", null).show()
+            }
+        }
+
+
+    }
+
+
+
+    //partie MQTT
+
+    private fun setMqttCallBack() {
+        mqttClient.setCallback(object : MqttCallbackExtended {
+            override fun connectComplete(b: Boolean, s: String) {
+                val snackbarMsg = "Connected to host:\n'$SOLACE_MQTT_HOST'."
+                Log.w("Debug", snackbarMsg)
+                Snackbar.make(findViewById(android.R.id.content), snackbarMsg, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show()
+            }
+            override fun connectionLost(throwable: Throwable) {
+                val snackbarMsg = "Connection to host lost:\n'$SOLACE_MQTT_HOST'"
+                Log.w("Debug", snackbarMsg)
+                Snackbar.make(findViewById(android.R.id.content), snackbarMsg, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show()
+            }
+            @Throws(Exception::class)
+            override fun messageArrived(topic: String, mqttMessage: MqttMessage) {
+                Log.w("Debug", "Message received from host '$SOLACE_MQTT_HOST': $mqttMessage")
+                textViewNumMsgs.text = ("${textViewNumMsgs.text.toString().toInt() + 1}")
+                val str: String = "------------"+ Calendar.getInstance().time +"-------------\n$mqttMessage\n${textViewMsgPayload.text}"
+                textViewMsgPayload.text = str
+            }
+
+            override fun deliveryComplete(iMqttDeliveryToken: IMqttDeliveryToken) {
+                Log.w("Debug", "Message published to host '$SOLACE_MQTT_HOST'")
+            }
+        })
+    }
+
+   /* override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }*/
+
+    override fun onDestroy() {
+        mqttClient.destroy()
+        super.onDestroy()
+    }
+
+}
+
---
+++ app/src/main/java/com/esprit/scannerqrcode/MessagingOptions.kt
@@ -0,0 +1,18 @@
+package com.esprit.scannerqrcode
+
+// Solace PubSub+ Broker Options
+
+// Fill in your Solace Cloud PubSub+ Broker's 'MQTT Host' and 'Password' options.
+// This information can be found under:
+// https://console.solace.cloud/services/ -> <your-service> -> 'Connect' -> 'MQTT'
+
+const val SOLACE_CLIENT_USER_NAME = "solace-cloud-client"
+const val SOLACE_CLIENT_PASSWORD = "4pgrrna52tbuqt93vfm2lilgdg"
+const val SOLACE_MQTT_HOST = "ssl://mr1qdyvoefhy7f.messaging.solace.cloud:8883"
+
+
+// Other options
+const val SOLACE_CONNECTION_TIMEOUT = 3
+const val SOLACE_CONNECTION_KEEP_ALIVE_INTERVAL = 60
+const val SOLACE_CONNECTION_CLEAN_SESSION = true
+const val SOLACE_CONNECTION_RECONNECT = true
